cd00a679291ffd70c9bca5484adbb4298930a19e
maaksimn <max.nevar1997@gmail.com>
2020-12-15 20:45:10 +0100

    comments were deleted

--- app.js
+++ app.js
@@ -8,25 +8,6 @@ for (let i = 0; i < addToCartBtns.length; i++) {
     
 )}
 
-// let productToFav = document.querySelectorAll(".to-favorite");
-// let count = new Array(productToFav.length).fill(0);
-
-// for (let i = 0; i < productToFav.length; i++) {
-//     productToFav[i].addEventListener("click", function() {
-//         count[i] += 1;
-//         if (count[i] % 2 == 1 ) {
-//             changeColorAndImageForFav(productToFav[i], "#2c71b8", "url(images/icon7.png)");
-//         } else {
-//             changeColorAndImageForFav(productToFav[i], "rgba(255, 255, 255, 0.5)", "url(images/icon5.png)");
-//         } 
-//     }   
-// )}
-
-// function changeColorAndImageForFav(button, color, imageUrl) {
-//     button.style.backgroundColor = color;
-//     button.style.backgroundImage = imageUrl;
-// }
-
 let productToFav = document.querySelectorAll(".to-favorite");
 let count = new Array(productToFav.length).fill(0);
 
